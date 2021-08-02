49ab23e6e285e43b85167d201aa1995159b1abc0
Tony <593356013@qq.com>
2021-08-02 16:40:44 +0800

    ros工作空间+aloam源代码

---
+++ build/A-LOAM/CMakeFiles/ascanRegistration.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ascanRegistration.dir/src/scanRegistration.cpp.o"
+  "/home/tony/aloam_ws/devel/lib/aloam_velodyne/ascanRegistration.pdb"
+  "/home/tony/aloam_ws/devel/lib/aloam_velodyne/ascanRegistration"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ascanRegistration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
